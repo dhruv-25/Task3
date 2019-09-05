8fb79934cb1414ec7f6bf65c208f3ee9e208321c
Dhruv Suthar <dhruvsuthar025@hotmail.com>
2019-09-05 12:25:11 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿create table Prduct
+(
+	ProductID int primary key identity,
+	ProductName varchar(max) null,
+	ProductPrice varchar(10) null,
+	ProductImage image,
+	ProductDesc varchar(max) null
+)
